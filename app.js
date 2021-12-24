cab43785c0d8621789b4d7cb55738c4589ef697e
DimaShumanskiy <dimaihfn@gmail.com>
2021-12-24 22:36:05 +0300

    connected database MongoDb

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-const express = require('express')
+
+//DimaShumanskiy -- Ridan123258
+
+const express = require('express')//require - скрипт-загрузчик
+const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.listen(5000, () => console.log('App has been started...')
+const PORT = config.get('port') || 5000
+
+async function start() {//обертка для метода connect
+    try {
+        await mongoose.connect(config.get('mongoUri'), { // connect - возвращает промис
+            // пораметры для успешной работы конеск
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`)) // настройка сервера
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)// в случае ошибки будет произведет выход
+    }
+}
+
+start()
