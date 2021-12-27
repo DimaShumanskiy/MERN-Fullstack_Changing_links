3bfca5ced8beb1fd075e365cb921be71cdb8a20f
DimaShumanskiy <dimaihfn@gmail.com>
2021-12-27 22:04:49 +0300

    layout AuthPage, registration block

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import {useRoutes} from "./routes";
 
 
 function App() {
-    const routes = useRoutes(true)
+    const routes = useRoutes(false)
     return (
             <div className='container'>
                 {/*<h1>Hello</h1>*/}
