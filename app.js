e6c07b1c3ace4093cf7d1abab9ff1a19c3ed897e
DimaShumanskiy <dimaihfn@gmail.com>
2022-01-09 21:31:36 +0300

    working draft

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')//require - скрипт-загрузчик
 const config = require('config')
 const mongoose = require('mongoose')
-const path = require('path')
+// const path = require('path')
 
 const app = express()
 
@@ -10,15 +10,16 @@ app.use(express.json({ extended: true}))
 // порты для обработки фронта (middleware)
 app.use('/api/auth', require('./routes/auth.routes')) // авторизация
 app.use('/api/link', require('./routes/link.routes'))// изменение ссылки
+app.use('/t/', require('./routes/redirect.routes'))
 
-if (process.env.NODE_ENV === 'production'){
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))// при запросе в корень проекта
-
-    //при других запросах
-    app.get('*', (req,res) =>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if (process.env.NODE_ENV === 'production'){
+//     app.use('/', express.static(path.join(__dirname, 'client', '/client/build')))// при запросе в корень проекта
+//
+//     //при других запросах
+//     app.get('*', (req,res) =>{
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', '/client/public/index.html'))
+//     })
+// }
 
 const PORT = config.get('port') || 5000
 
